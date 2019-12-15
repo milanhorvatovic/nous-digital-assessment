30c960cba75a17ffbfdc1002e785c146d1043349
Milan Horvatovic <imsoft.labs@gmail.com>
2019-12-15 09:14:11 +0100

    [+] empty project

---
+++ Nous Digital AssessmentTests/Nous_Digital_AssessmentTests.swift
@@ -0,0 +1,34 @@
+//
+//  Nous_Digital_AssessmentTests.swift
+//  Nous Digital AssessmentTests
+//
+//  Created by worker on 15/12/2019.
+//  Copyright © 2019 Milan Horvatovic. All rights reserved.
+//
+
+import XCTest
+@testable import Nous_Digital_Assessment
+
+class Nous_Digital_AssessmentTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
